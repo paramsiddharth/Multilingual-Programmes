8e3d2eb0218b66ce15826ca6f64944afae6c3ea4
Param Siddharth <30315706+paramsiddharth@users.noreply.github.com>
2020-03-26 20:35:24 +0530

    Create main.cpp

---
+++ Factorial using Recursion/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+long long int fact(long long int x) {
+    return (x<2)?1:x*fact(x-1);
+}
+
+int main() {
+    long long int y;
+    cin >> y;
+    cout << fact(y);
+    return 0;
+}
