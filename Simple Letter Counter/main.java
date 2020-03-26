8365410d15bb9a967ae7039032d6c992dd840eeb
Param Siddharth <30315706+paramsiddharth@users.noreply.github.com>
2020-03-26 20:22:58 +0530

    Create main.java

---
+++ Simple Letter Counter/main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Program
+{
+    public static void main(String[] args) {
+        String a;
+        Scanner s = new Scanner(System.in);
+        a = s.nextLine().toLowerCase();
+        int[] x = new int[256];
+        for (int i=0; i<a.length(); i++)
+            x[a.charAt(i)]++;
+        for (int i=97;i<123;i++)
+            System.out.println((char)(i)+" -> "+x[i]);
+    }
+}
